e19783983b5083b4f7fa8bac86ce3533e155b966
Zeptar <84585164+ZeptarTeam@users.noreply.github.com>
2021-10-09 22:02:55 +0200

    Add files via upload

---
+++ pfpgenerator/pfpgenerator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pfpgenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient web = new WebClient();
+            String name = web.DownloadString("https://api.leaked.wiki/randomuser?json=no");
+
+               
+                richTextBox1.Text = "";
+                richTextBox1.AppendText($"{name}");
+                
+              
+           
+        }
+    }
+}
